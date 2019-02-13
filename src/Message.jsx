278a6f52583fba0ed400f32a215b4791a87938f3
ShahjamalMalik <shahjamalmalik@gmail.com>
2019-02-13 21:08:44 +0000

    Added CSS and made components files for my components so they return the html needed for ChattyApp.

---
+++ src/Message.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+
+    return (
+      <div className="message">
+        <span className="message-username">Anonymous1</span>
+        <span className="message-content">It's lunchtime</span>
+      </div>
+
+    );
+  }
+}
+
+export default Message; 
